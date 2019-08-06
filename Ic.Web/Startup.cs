cb8a0872c53d80acfa251b120351051f207cb5d1
liuxu@Company <lx19920404@hotmail.com>
2019-08-06 16:17:36 +0800

    0806 Ic.Web项目加入

---
+++ Ic.Web/Startup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Ic.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
+
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+        public Task MyDele1(HttpContext context)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine(context.Request.Headers.ToArray().ToString());
+                context.Response.WriteAsync("First Middleware");
+            });
+        }
+        public Task MyDele2(HttpContext context)
+        {
+            return Task.Run(() =>
+            {
+                context.Response.WriteAsync("Second Middleware");
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Use(request =>
+            {
+                RequestDelegate dele = new RequestDelegate(MyDele1);
+                return dele;
+            });
+            app.Use(request =>
+            {
+                RequestDelegate dele = new RequestDelegate(MyDele2);
+                return dele;
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
