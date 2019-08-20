3ebc7c537f561b8a89a8f8514f61d46368fab69c
liuxu@company <693927621@qq.com>
2019-08-21 00:54:29 +0800

    加入服务提供者项目，包含token验证，consul注册，待重构。

---
+++ Ic.ClientService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ic.ClientService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config1 = new ConfigurationBuilder().AddCommandLine(args).Build();
+            string ip = config1["ip"];
+            string port = config1["port"];
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseUrls($"http://{ip}:{port}");
+        }
+            
+    }
+}
