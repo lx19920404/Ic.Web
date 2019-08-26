8c0f44035cf2e8d25781308a07daa9f3411b08b6
liuxu@company <693927621@qq.com>
2019-08-26 22:52:05 +0800

    网关内加入验证服务路由。

--- Ic.GateWay/Startup.cs
+++ Ic.GateWay/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
 
 namespace Ic.GateWay
 {
@@ -54,7 +55,7 @@ namespace Ic.GateWay
                 .AddIdentityServerAuthentication("ProductServiceKey", isaOptProduct);
 
             //Ocelot
-            services.AddOcelot(Configuration).AddConsul();
+            services.AddOcelot(Configuration).AddConsul().AddPolly();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
