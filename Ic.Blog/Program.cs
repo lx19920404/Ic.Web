bfa0c1b03d176fc050994319effe113b99828d89
iCyrene@Legion <693927621@qq.com>
2020-01-10 16:06:03 +0800

    加入Blog项目。

---
+++ Ic.Blog/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Ic.Blog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) 
+        {
+            var config1 = new ConfigurationBuilder().AddCommandLine(args).Build();
+            string ip = config1["ip"];
+            string port = config1["port"];
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseUrls($"http://{ip}:{port}");
+        }
+            
+    }
+}
