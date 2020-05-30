b923083f86bb5d3a2281c8d897bd32093e32828e
刘旭 <liuxu@piesat.cn>
2020-05-30 11:32:02 +0800

    添加认证测试。

---
+++ Ic.Blog/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ic.Blog.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test Service is OK!!!");
+        }
+    }
+}
